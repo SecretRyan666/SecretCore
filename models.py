eb6d9b2c42526b141ffc56dc6ea1fd312a081ccd
SecretRyan666 <hyperedmoon666@gmail.com>
2026-02-16 00:57:39 +0900

    update models with analysis record table

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
 from database import Base
 
 class User(Base):
@@ -9,3 +11,19 @@ class User(Base):
     password = Column(String)
     is_approved = Column(Boolean, default=False)
     is_admin = Column(Boolean, default=False)
+
+    analyses = relationship("AnalysisRecord", back_populates="owner")
+
+
+class AnalysisRecord(Base):
+    __tablename__ = "analysis_records"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String)
+    total_rows = Column(Integer)
+    total_columns = Column(Integer)
+    columns = Column(Text)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="analyses")
